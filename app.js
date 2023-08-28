62f38454210f034cc0bcff2a0a7611451a0a7d50
Oleksandr <OleksandrW1@gmail.com>
2023-08-29 01:50:21 +0300

    app.js

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
 // 1
-
 let a = prompt("Введіть перше число");
 let b = prompt("Введіть друге число");
 
-let arr = [a, b];
-let max = arr[0];
-
-arr.forEach(function (elem, index) {
-  if (index > 0) {
-    if (max < elem) {
-      max = elem;
-    }
-    alert(`Більше число з двох буде ${max[0]}`);
-  }
-});
-
-console.log(max);
-
-// 2
-
-let Name = prompt("Введіть ваше ім'я");
-let age = prompt("Введіть ваш вік");
-
-if (age >= 59 || age <= 60) {
-  alert(`Шановний ${Name}, вам на пенсію через років`);
+if (a > b) {
+  alert(`Більше ${a}`);
 } else {
-  alert(`Шановний ${Name}, ви вже на пенсії`);
+  alert(`Більше ${b}`);
 }
 
 
+// 1.1
 
+// let arr = [a, b];
+// let max = arr[0];
+
+// arr.forEach(function (elem, index) {
+//   if (index > 0) {
+//     if (max < elem) {
+//       max = elem;
+//     }
+//     alert(`Більше число з двох буде ${max[0]}`);
+//   }
+// });
+// console.log(max);
+
+// 2
+
+const Name = prompt("Введіть ваше ім'я");
+const year = +prompt("Введіть свій рік народження");
+const age = 2023 - year;
+const work = 60 - age;
+alert("Вам:  " + age + "  років");
+
+  if (work >= 60) {
+    alert(`Шановний ${Name}, ви вже на пенсії`); 
+  } else {
+    alert(`Шановний ${Name}, вам на пенсію через ${work}  років`);
+  }
